f498d03173f9d6cb3a27e796852ce0d3fbc8410d
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-09-05 20:49:42 +0300

    fix moviServise

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,16 +3,21 @@ import type { Movie } from '../types/movie';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 
+// Опис структури відповіді TMDB
+interface FetchMoviesResponse {
+  results: Movie[];
+}
+
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
   try {
-    const response = await axios.get(`${BASE_URL}/search/movie`, {
+    const response = await axios.get<FetchMoviesResponse>(`${BASE_URL}/search/movie`, {
       params: { query },
       headers: {
         Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
       },
     });
 
-    return response.data.results as Movie[];
+    return response.data.results;
   } catch {
     throw new Error('Failed to fetch movies');
   }
